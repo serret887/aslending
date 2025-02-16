bb89ee82e04abe000222a868bb363c78853b6c51
Ale <Manuel.AlejandroDiazSerret@alphaomega.com>
2025-02-15 22:06:14 -0500

    deprecation on internatioanlization

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,7 +1,8 @@
 import {getRequestConfig} from 'next-intl/server';
 import i18nConfig from './config';
  
-export default getRequestConfig(async ({locale}) => {
+  export default getRequestConfig(async ({requestLocale}) => {
+    let locale = await requestLocale;
   if (!i18nConfig.locales.includes(locale as any)) {
     return { messages: (await import(`../../messages/${i18nConfig.defaultLocale}.json`)).default };
   }
