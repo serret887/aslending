6d87edac52f1374d378720c6b4842ea62ba1f1b4
Ale <Manuel.AlejandroDiazSerret@alphaomega.com>
2025-02-16 01:18:02 -0500

    removing unnecessary tabs

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -57,49 +57,15 @@ const data = {
       url: "/dashboard",
       icon: Home,
       isActive: true,
-    },
-    {
-      title: "Models",
-      url: "#",
-      icon: Bot,
       items: [
         {
-          title: "Genesis",
-          url: "#",
+          title: "Analytics",
+          url: "/dashboard/analytics",
         },
-        {
-          title: "Explorer",
-          url: "#",
-        },
-        {
-          title: "Quantum",
-          url: "#",
-        },
+        
       ],
     },
-    {
-      title: "Documentation",
-      url: "#",
-      icon: BookOpen,
-      items: [
-        {
-          title: "Introduction",
-          url: "#",
-        },
-        {
-          title: "Get Started",
-          url: "#",
-        },
-        {
-          title: "Tutorials",
-          url: "#",
-        },
-        {
-          title: "Changelog",
-          url: "#",
-        },
-      ],
-    },
+   
     {
       title: "Settings",
       url: "#",
